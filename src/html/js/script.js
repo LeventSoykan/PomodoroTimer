10d40c5b208c180c5f0cca39db311b17ee7ea19d
LeventSoykan <levent_soykan@yahoo.com>
2024-02-13 14:22:09 +0300

    Create alarm and update ui with bootstrap

--- src/html/js/script.js
+++ src/html/js/script.js
@@ -1,41 +1,47 @@
-let startingMinutes = 25
-let time = startingMinutes * 60
-let isRunning = false
+let startingMinutes = 25;
+let time = startingMinutes * 60;
+let isRunning = false;
 let clock;
 let short_duration = 5;
 let long_duration = 15;
 let pomodoro_duration = 25;
 
-const countdownEl = document.getElementById('countdown')
-const counterButton  = document.getElementById('start')
-const long_input = document.getElementById('long')
-const pomodoro_input = document.getElementById('pomodoro')
-const short_input = document.getElementById('short')
-const form = document.getElementById('form')
-const periodSelect = document.getElementById('period-selection');
+const countdownEl = document.getElementById('countdown');
+const counterButton = document.getElementById('start');
+const long_input = document.getElementById('long');
+const pomodoro_input = document.getElementById('pomodoro');
+const short_input = document.getElementById('short');
+const form = document.getElementById('form');
+const periodSelect = document.querySelector('.button-select');
+const alarmSound = new Audio('static/alarm.mp3'); // Path to your alarm sound file
+
+update_timer();
+updateSettingsModal();
 
 function update_timer() {
     const minutes = Math.floor((time / 60));
     let seconds = time % 60;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+
+    if (time === 0) {
+        alarmSound.play();
+    }
 }
 
-update_timer()
-
 function updateCountDown() {
-    if (time>0) {
+    if (time > 0) {
         isRunning = true;
-        counterButton.innerHTML = 'Pause'
+        counterButton.innerHTML = 'Pause';
         time--;
-        update_timer()
+        update_timer();
     } else {
         isRunning = false;
-        counterButton.innerHTML = 'Start'
-        clearInterval(clock)
-        time = startingMinutes * 60
-        update_timer()
+        counterButton.innerHTML = 'Start';
+        clearInterval(clock);
+        time = startingMinutes * 60;
+        update_timer();
     }
 }
 
@@ -43,7 +49,7 @@ function start() {
     if (isRunning) {
         clearInterval(clock);
         isRunning = false;
-        counterButton.innerHTML = 'Resume'
+        counterButton.innerHTML = 'Resume';
     } else {
         clock = setInterval(updateCountDown, 1000);
     }
@@ -55,53 +61,39 @@ function period(element) {
             startingMinutes = short_duration;
             break;
         case 'Long Break':
-            startingMinutes = long_duration
+            startingMinutes = long_duration;
             break;
         case 'Pomodoro':
-            startingMinutes = pomodoro_duration
+            startingMinutes = pomodoro_duration;
             break;
     }
-    time = startingMinutes*60
+    time = startingMinutes * 60;
     isRunning = false;
-    update_timer()
-    clearInterval(clock)
-    counterButton.innerHTML = 'Start'
+    update_timer();
+    clearInterval(clock);
+    counterButton.innerHTML = 'Start';
 }
 
-function select(selection, box) {
-    if (selection > 60) {
-        alert('Please select a value below 60 minutes')
-    } else {
+document.getElementById('settings').addEventListener('click', function () {
+    $('#myDialog').modal();
+})
 
-    }
-}
-
-function settings() {
-    document.getElementById("myDialog").open = true;
-}
-
-function closeDialog() {
-    document.getElementById("myDialog").open = false;
+function updateSettingsModal() {
+    document.getElementById('short').value = short_duration;
+    document.getElementById('pomodoro').value = pomodoro_duration;
+    document.getElementById('long').value = long_duration;
 }
 
 function save_settings() {
-    pomodoro_duration = pomodoro_input.innerHTML
-    short_duration = pomodoro_input.innerHTML
-    long_duration = pomodoro_input.innerHTML
-    console.log(pomodoro_duration)
-    // closeDialog()
+    pomodoro_duration = parseInt(pomodoro_input.value);
+    short_duration = parseInt(short_input.value);
+    long_duration = parseInt(long_input.value);
+    console.log(pomodoro_duration);
+    updateSettingsModal();
+    $('#myDialog').modal('hide');
 }
 
-
-form.addEventListener("submit", function(e) {
-  e.preventDefault();
-  const data = new FormData(form);
-  pomodoro_duration = data.get('pomodoro');
-  long_duration = data.get('long');
-  short_duration = data.get('short');
-  console.log(short_duration)
-})
-
-periodSelect.addEventListener('click',  ({target}) => {
-    period(target)
-})
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    save_settings();
+});
